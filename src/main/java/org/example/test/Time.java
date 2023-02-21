ab23c0aea8e48de7afa059461f9a46273ad9196c
ZhangHongyan <13436409644@163.com>
2023-02-21 13:43:32 +0800

    update

---
+++ src/main/java/org/example/test/Time.java
@@ -0,0 +1,10 @@
+package org.example.test;
+
+public class Time {
+    static  String a  = "wang";
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println(a.length());
+
+    }
+}
